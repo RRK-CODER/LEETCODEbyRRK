a25399185d9bc0a12ec974a4c89a1cb5c7c90281
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-04-06 21:24:14 +0530

    Time: 200 ms (5.36%), Space: 5.8 MB (79.92%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        int memo[100];
+      	memset(memo, -1, sizeof(memo));
+       
+            if(memo[n]==-1)
+            {
+                int res;
+                if(n==0 || n==1)
+                    res=n;
+                else
+                {res=fib(n-1)+fib(n-2);}
+                    
+                memo[n]=res;
+            }
+        
+    return memo[n];
+    }
+};
