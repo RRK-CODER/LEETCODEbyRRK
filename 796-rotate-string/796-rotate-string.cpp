3c9afa4bb0a6ab3395f9fcbaacb92091ce647e7a
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-05-03 13:42:04 +0530

    Time: 3 ms (38.82%), Space: 6 MB (97.88%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length())
+            return false;
+        return ((s+s).find(goal)!=string::npos);
+    }
+};
