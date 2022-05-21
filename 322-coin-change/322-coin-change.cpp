711c0bbebac653bc860f624ed0025b31d00ccb08
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-05-21 11:04:22 +0530

    Time: 84 ms (79.77%), Space: 14.4 MB (68.59%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,12 +4,8 @@ public:
         vector<int> A(amount+1, amount +1);
         A[0]=0;
         for(int coin: coins)
-        {
-            for(int i=coin; i<=amount; i++)
-            {
-                A[i]=min(A[i], A[i-coin]+1);
-            }
-        }
+        {for(int i=coin; i<=amount; i++)
+      A[i]=min(A[i], A[i-coin]+1);}
         return A[amount]> amount ? -1 : A[amount];
     }
 };
