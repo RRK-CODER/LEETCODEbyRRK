2f2417f77277a73553a7fb0947a5cd17152a0502
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-05-29 12:41:32 +0530

    Time: 18 ms (32.29%), Space: 8.7 MB (31.04%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack <int> st;
+        char sign='+';
+        long long int res=0, tmp=0;
+        for(int i=0; i <s.size();i++)
+        {
+            if(isdigit(s[i]))
+                tmp= 10 * tmp + s[i] - '0';
+            if(!isdigit(s[i])  && !isspace(s[i]) || i==s.size()-1){
+                if(sign=='-')
+                    st.push(-tmp);
+                else if(sign=='+')
+                    st.push(tmp);
+            else 
+            {
+                long long int num;
+                if(sign=='*')
+                    num=st.top()*tmp;
+                else
+                    num=st.top()/tmp;
+                    st.pop();
+                st.push(num);
+            }
+            sign=s[i];
+            tmp=0;
+        }
+        }
+        while(!st.empty()){
+res+=st.top();
+        st.pop();}
+        return res;
+    }
+};
