42d33bafd0d0a34bf86541e3c865c97ec9531044
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-07-26 09:15:39 +0530

    Time: 44 ms (37.32%), Space: 29 MB (22.07%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -19,7 +19,6 @@ return (s->val==t->val && isIdentical(s->left,t->left) && isIdentical(s->right, 
     }
     bool isSubtree(TreeNode* s, TreeNode* t) {
    if(!s) return false;
-  if(!t) return true;
         if(isIdentical(s,t)) return true;
         return isSubtree(s->left,t) || isSubtree(s->right,t);
     }
