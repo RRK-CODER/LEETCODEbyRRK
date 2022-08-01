aed12436beedfb930019a921e8bbd8fe6d7afdda
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-08-01 12:41:31 +0530

    Time: 100 ms (42.04%), Space: 6 MB (70.00%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(int e, int f)
+    {
+        int dp[f+1][e+1];
+        for(int i=1; i<=e ; i++){
+    dp[1][i]=1;
+            dp[0][i]=0;
+}
+        for(int j=1; j<=f; j++)
+            dp[j][1]=j;
+        for(int i=2; i<=f; i++)
+        {
+            for(int j=2; j<=e; j++)
+            {
+                dp[i][j]=INT_MAX;
+                  for(int x=1; x<=i; x++)
+dp[i][j]=min(dp[i][j],(1+max(dp[x-1][j-1],dp[i-x][j])));
+            }
+        }
+      return dp[f][e];
+}
+    int twoEggDrop(int f) {
+      int e=2;
+        return solve(e,f);
+    }
+};
