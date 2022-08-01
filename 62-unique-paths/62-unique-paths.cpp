3bf1a1e8294456ce39717a30d6d10c1125df62bb
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-08-02 00:03:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.04%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N=m+n-2;
+        int r=n-1;
+        double res=1;
+        for(int i=1; i<=r; i++)
+            res=res*(N-r+i)/i;
+        return res;
+    }
+};
