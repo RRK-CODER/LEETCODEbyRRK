c5f3a966dbdc58901ddc2f3377cac0ba536707c6
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-08-06 09:54:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (58.91%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pig=0; 
+        while(pow(minutesToTest/minutesToDie+1,pig)<buckets)
+        {
+            pig+=1;
+        }
+        return pig;
+    }
+};
