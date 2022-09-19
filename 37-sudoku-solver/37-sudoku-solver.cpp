aea88c2bd4d71ddb6828e6fcf5965551b4000a4f
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-09-19 21:28:11 +0530

    Time: 84 ms (12.98%), Space: 6.4 MB (51.38%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+        bool isValid(char c,vector<vector<char>>& board,int row,int col){
+        for(int i=0;i<board.size();i++){
+            if(board[row][i] == c || board[i][col] == c){
+                return false;
+            }
+            
+            if(board[3*(row/3)+i%3][3*(col/3)+i/3] == c){
+                return false;
+            }
+        }
+        return true;
+    }
+   bool fillSudoku(vector<vector<char>>& board){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j] == '.'){
+                    
+                    for(char c='1';c<='9';c++){
+                        if(isValid(c,board,i,j)){
+                            board[i][j] = c;
+                            if(fillSudoku(board) == true){
+                                return true;
+                            }
+                            else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        fillSudoku(board);
+    }
+};
